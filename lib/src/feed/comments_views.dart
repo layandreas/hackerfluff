c5ac462099b734d5d6a4d4150bb9454fbd6f3ce0
Andreas Lay <xxx@gmail.com>
2024-01-06 03:38:35 +0100

    Added hiding of comments

--- lib/src/feed/comments_views.dart
+++ lib/src/feed/comments_views.dart
@@ -2,23 +2,39 @@ import 'package:flutter/material.dart';
 import 'comment_model.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
-class SingleCommentsView extends StatelessWidget {
+class SingleCommentsView extends StatefulWidget {
   final CommentModel comment;
   final bool isParentWidget;
   const SingleCommentsView(
       {super.key, required this.comment, this.isParentWidget = false});
 
   @override
+  State<SingleCommentsView> createState() => _SingleCommentsViewState();
+}
+
+class _SingleCommentsViewState extends State<SingleCommentsView> {
+  bool hideChildren = false;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: isParentWidget
+      padding: widget.isParentWidget
           ? const EdgeInsets.only(left: 0)
           : const EdgeInsets.only(left: 40.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (comment.text != null) Card(child: HtmlWidget(comment.text ?? '')),
-          for (final child in comment.children!)
+          if (widget.comment.text != null)
+            GestureDetector(
+              child: Card(
+                  child: hideChildren
+                      ? const Text('Hidden')
+                      : HtmlWidget(widget.comment.text ?? '')),
+              onTap: () => setState(() {
+                hideChildren = !hideChildren;
+              }),
+            ),
+          for (final child in hideChildren ? [] : widget.comment.children!)
             SingleCommentsView(comment: child)
         ],
       ),
