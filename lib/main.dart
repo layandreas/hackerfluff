578245f5f4bea561f85ada4f365adfd43db5ed3a
Andreas Lay <xxx@gmail.com>
2024-01-07 11:50:06 +0100

    removed duplicate import

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:flutter/material.dart';
 import 'package:json_theme/json_theme.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
