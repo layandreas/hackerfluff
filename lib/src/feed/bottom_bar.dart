0230171e34e0ed9ab20ef7850b33863967a3bdfb
Andreas Lay <andreas.lay@gmail.com>
2024-01-19 19:22:30 +0100

    Added bottom bar widget

---
+++ lib/src/feed/bottom_bar.dart
@@ -0,0 +1,47 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  final Widget child;
+  const BottomBar({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        child,
+        ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 80,
+              color: Theme.of(context).colorScheme.background.withOpacity(0.9),
+              child: const Row(children: [
+                Expanded(
+                    child: Icon(
+                  Icons.home_outlined,
+                  size: 30,
+                )),
+                Expanded(
+                    child: Icon(
+                  Icons.bookmark_outline_rounded,
+                  size: 30,
+                )),
+                Expanded(
+                    child: Icon(
+                  Icons.settings_outlined,
+                  size: 30,
+                ))
+              ]),
+
+              //Colors.black.withOpacity(0.8),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
