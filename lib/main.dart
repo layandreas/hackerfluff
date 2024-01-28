b64894dd87017dc2a2f9f006c3db72af6eead3cc
Andreas Lay <xxx@gmail.com>
2024-01-28 21:18:00 +0100

    Cleaning up

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,12 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:json_theme/json_theme.dart';
-import 'package:flutter/services.dart';
-import 'dart:convert';
 import 'src/settings/settings_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Map<ThemeSetting, ThemeData> themes = {};
 
-  for (final themePath in [
-    ('assets/appainter_theme_oled_dark.json', ThemeSetting.oledDark),
-    ('assets/appainter_theme_light.json', ThemeSetting.light),
-    ('assets/appainter_theme_blue.json', ThemeSetting.blue)
-  ]) {
-    final themeStr = await rootBundle.loadString(themePath.$1);
-    final themeJson = jsonDecode(themeStr);
-    final theme = ThemeDecoder.decodeThemeData(themeJson)!;
-    themes[themePath.$2] = theme;
-  }
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
