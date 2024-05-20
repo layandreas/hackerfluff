a8aa507ba8bf15dd0902b6535b9cddd5c688758d
Andreas Lay <xxx@gmail.com>
2024-05-20 22:57:34 +0200

    Added sqflite web

--- lib/src/storage/db_provider.dart
+++ lib/src/storage/db_provider.dart
@@ -1,16 +1,26 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:developer';
+import 'package:flutter/foundation.dart';
 
 // Necessary for code-generation to work
 part 'db_provider.g.dart';
 
 @Riverpod(keepAlive: true)
 Future<Database> database(DatabaseRef ref) async {
-  var databasesPath = await getLibraryDirectory();
-  String path = join(databasesPath.path, 'hacker.db');
+  late String path;
+
+  if (kIsWeb) {
+    // Change default factory on the web
+    databaseFactory = databaseFactoryFfiWeb;
+    path = 'my_web_web.db';
+  } else {
+    final databasesPath = await getLibraryDirectory();
+    path = join(databasesPath.path, 'hacker.db');
+  }
 
   log('Database path is: $path');
 
