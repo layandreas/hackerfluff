3478476d76e1bb4acd2d267e6b1b946fef07c91f
Andreas Lay <andreas.lay@gmail.com>
2024-05-29 10:28:40 +0200

    Added platform check for setAppGroupId

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'src/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:home_widget/home_widget.dart'; // Add this import
+import 'dart:io' show Platform;
 
 const String appGroupId = 'group.com.layandreas.hackerfluff';
 const String iOSWidgetName = 'HackerfluffWidgets';
@@ -9,7 +10,9 @@ const String iOSWidgetName = 'HackerfluffWidgets';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  HomeWidget.setAppGroupId(appGroupId);
+  if (Platform.isIOS) {
+    HomeWidget.setAppGroupId(appGroupId);
+  }
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
